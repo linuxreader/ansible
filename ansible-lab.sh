42b6705cdea525336388587d3fc998a5d3d4de12
linuxreader <tdavetech@gmail.com>
2025-12-24 08:09:21 -0700

    consolidate ansible-labs

--- ansible-lab.sh
+++ ansible-lab.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-ansible-playbook kvm_provision.yaml -e vm=podman-01 -e ip_addr=192.168.122.3
-ansible-playbook kvm_provision.yaml -e vm=web-01 -e ip_addr=192.168.122.5
+ansible-playbook kvm_provision.yaml -K -e vm=podman-01 -e ip_addr=192.168.122.3
 
